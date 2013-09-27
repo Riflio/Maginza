6e11426c8b82f38d1bf7eef2f62245f5fdbbfd6d
Riflio <2me.pavelk@gmail.com>
2013-09-27 10:56:38 +0400

    Разрабатываю корзину

--- maginza.php
+++ maginza.php
@@ -20,8 +20,6 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-require_once 'classes/interfaces.php';
-
 require_once 'classes/options.php';
 require_once 'classes/formatter.php';
 require_once 'classes/meta.php';  require_once 'classes/metasettings.php';
