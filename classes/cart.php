995b6d745feae9acaa6c2d2e6201dc9a6982c407
Riflio <2me.pavelk@gmail.com>
2013-09-28 20:33:37 +0400

    Работаю над корзиной

--- classes/cart.php
+++ classes/cart.php
@@ -47,7 +47,7 @@ class Cart extends Order {
                         echo $comb['combination'];
                     echo '</div>';
                     echo '<div class="countandprice">';
-                       echo '<span class="price">'; $this->theMetaValue($lot, 'Price', 'cart'); echo '</span>';
+                       echo '<span class="dprice">'; $this->theMetaValue($lot, 'Price', 'cart'); echo '</span>';
                        echo '<span class="x">×</span>';
                        echo '<span class="count">'; $this->theMetaValue($lot, 'Quantity',  'cart-'.$itemID); echo '</span>';
                        echo '<span class="eq">=</span>';
