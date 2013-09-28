c6661fa0c3483fc0c50e68ff58b459ba54304846
Riflio <2me.pavelk@gmail.com>
2013-09-29 00:38:31 +0400

    Работаю над корзиной

--- classes/orderitem.php
+++ classes/orderitem.php
@@ -66,7 +66,7 @@ class OrderItem extends Combinations{
         }
         $metaOpts=array_merge($orderItemMetaOptions, $customMetaOptions);
 
-        return  $metaOpts;
+        return  $orderItemMetaOptions;
     }
 
     /**
