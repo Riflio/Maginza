d743d319bc94f2fb4aeebce897f35598566459d2
Riflio <2me.pavelk@gmail.com>
2013-09-29 02:44:05 +0400

    Работаю над формулой вычисления

--- classes/cart.php
+++ classes/cart.php
@@ -56,7 +56,7 @@ class Cart extends Order {
                     echo '</div>';
 
                     echo '<div class="actionbtns">';
-                        Cart::theButton('delete', 'Удалить', $itemID);
+                        Cart::theButton('delete', 'Удалить', "#");
                     echo '</div>';
 
                 echo '</div>';
