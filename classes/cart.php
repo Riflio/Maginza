efb67d16304e98efe66f06d158f5c0bc5b260b3a
Riflio <2me.pavelk@gmail.com>
2013-09-29 05:28:18 +0400

    Работаю над охранением корзины

--- classes/cart.php
+++ classes/cart.php
@@ -56,7 +56,7 @@ class Cart extends Order {
                     echo '</div>';
                     echo '<div class="countandprice">';
                        echo '<span class="dprice">'; $this->theMetaValue($lot, 'Price', 'cart'); echo '</span>';
-                       echo '<span class="x">x</span>';
+                       echo '<span class="x">&times;</span>';
                        echo '<span class="count">'; $this->theMetaValue($lot, 'Quantity',  'cart-'.$itemID); echo '</span>';
                        echo '<span class="eq">=</span>';
                        echo '<span class="cost">'; echo Formatter::format('price', '', $this->getItemTotalPrice()); echo '</span>';
