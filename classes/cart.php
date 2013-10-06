c16215a1fb6a790fedb0a64cbffe498fc2454680
Riflio <2me.pavelk@gmail.com>
2013-10-07 03:57:42 +0400

    Добавляю просмотр заказов в админке

--- classes/cart.php
+++ classes/cart.php
@@ -232,6 +232,7 @@ class Cart extends Order {
         $torder=Options::$table_order;
         $orders=$wpdb->get_results($wpdb->prepare("SELECT * FROM {$torder} WHERE userID=%1", Buyer::ID()));
         return  $orders;
+        var_dump($wpdb->prepare("SELECT * FROM {$torder} WHERE userID=%1", Buyer::ID()));
     }
 
     /**
