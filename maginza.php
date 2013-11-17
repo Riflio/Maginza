663931b735d03aff09a3246bdf136308de37d4d8
PavelK <2me.pavelk@gmail.com>
2013-11-17 04:29:40 +0400

    правки

--- maginza.php
+++ maginza.php
@@ -151,9 +151,9 @@ class Maginza extends Options  {
 		if( is_search() ) {
 			$qfindkeys=$wpdb->get_results('SELECT optName FROM '.Options::$table_meta_options.' WHERE optForSearch=1');
 			$findkeys=array();	
-			foreach ($qfindkeys as $findkey) { $findkeys[]= '\''.$findkey->optName.'\''; }
+			foreach ($qfindkeys as $findkey) { $findkeys[]= $findkey->optName; }
 			$findkeys=implode(',', $findkeys);			
-			$where = preg_replace('/post_title LIKE \'(.*?)\'\)/', 'post_title LIKE  \'$1\') OR ( (FIND_IN_SET(wp_maginzameta.meta_key, '.$findkeys.')) AND  (wp_maginzameta.meta_value LIKE \'$1\') ) ', $where );
+			$where = preg_replace('/post_title LIKE \'(.*?)\'\)/', 'post_title LIKE  \'$1\') OR ( (FIND_IN_SET(wp_maginzameta.meta_key, \''.$findkeys.'\')) AND  (wp_maginzameta.meta_value LIKE \'$1\') ) ', $where );
 			var_dump($where);
 		}
 		return $where;
