95742620f5cdbc03bc83e5ad1c5ea687eeedaf31
PavelK <2me.pavelk@gmail.com>
2014-04-23 03:32:31 +0400

    Опять забывчивость моя

--- classes/meta.php
+++ classes/meta.php
@@ -138,7 +138,7 @@ class Meta extends Formatter {
 		foreach($options as $option) {
 			if (in_array($option->optName, $exclude))	continue;				
 			$metaVal=$this->getMetaValue($lot, $option->optName);			
-			$this->processMetaOption($option, $metaVal,  $formName, $lot->ID);
+			$this->processMetaOption($option, $metaVal,  $formName, true, $lot->ID);
 		}		
 		return true;
 	}
